890269b12bc92b7703d0c9a79468938f81d88edb
Hazwan Z <zwanrs@gmail.com>
2019-11-14 11:49:54 -0500

    PSSC 3, 4, & 5 - Some memory issues

---
+++ STM32L0_PCB/Inc/G350.h
@@ -0,0 +1,16 @@
+#ifndef __G350_H
+#define __G350_H
+
+#include <stdint.h>
+#include <string.h>
+#include "stm32l0xx_hal.h"
+
+int GSM_Init(UART_HandleTypeDef *);
+int Attempt_GSM_UART_Transmit_Wait(UART_HandleTypeDef *, char *, int, int);
+int GSM_UART_Transmit_Wait(UART_HandleTypeDef *, char *, int, int);
+int OK_PRESENCE(char *);
+int GSM_Send_Text(UART_HandleTypeDef *, char *, char *);
+int Attempt_GSM_UART_Transmit_Msg(UART_HandleTypeDef *, char *, char *);
+int GSM_UART_Transmit_Msg(UART_HandleTypeDef *, char *, int);
+
+#endif /* __G350_H */
--- STM32L0_PCB/Src/main.c
+++ STM32L0_PCB/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -43,6 +43,9 @@
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c2;
 
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -50,7 +53,9 @@ I2C_HandleTypeDef hi2c2;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_I2C2_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -89,16 +94,68 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_I2C2_Init();
+  MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+/*
+  // --- BATTERY STUFF ---
+  HAL_Delay(500);
 
   FG_I2C_Setup(&hi2c2);
+
+  HAL_Delay(5000);
+
   uint16_t battery_percentage = FG_I2C_Read_SOC(&hi2c2);
-  //uint16_t voltage_reading = FG_I2C_Read_Voltage(&hi2c2);
 
   if (battery_percentage)
   {
       asm("nop");
+  }
+
+  char full_msg[14] = {"1, ALIVE, "};
+
+  int bat_percent = (int) (unsigned) battery_percentage;
+
+  if (bat_percent < 10) // Battery percentage is 1 digit
+  {
+      full_msg[10] = bat_percent + '0';
+      full_msg[11] = '\0';
+  }
+
+  else if (bat_percent < 100) // Battery percentage is 2 digits
+  {
+      int dig1 = bat_percent / 10;
+      int dig2 = bat_percent % 10;
+
+      full_msg[10] = dig1 + '0';
+      full_msg[11] = dig2 + '0';
+      full_msg[12] = '\0';
+  }
+  else // Battery is at 100%
+  {
+      full_msg[10] = '1';
+      full_msg[11] = '0';
+      full_msg[12] = '0';
+      full_msg[13] = '\0';
+  }
+*/
+  // --- GSM STUFF ---
+  if (GSM_Init(&huart2) == 1)
+  {
+      HAL_Delay(5000);
+      HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+      int i = GSM_Send_Text(&huart2, "+16782273831", "This is Sparta!");
+      if (i)
+      {
+          while(1)
+          {
+              HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+              HAL_Delay(500);
+              HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+              HAL_Delay(500);
+          }
+      }
   }
 
   /* USER CODE END 2 */
@@ -107,9 +164,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-      HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
     /* USER CODE END WHILE */
-
+    HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -123,6 +179,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Configure the main internal regulator output voltage 
   */
@@ -150,6 +207,12 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
@@ -202,6 +265,57 @@ static void MX_I2C2_Init(void)
 }
 
 /**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/** 
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void) 
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel4_5_6_7_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel4_5_6_7_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel4_5_6_7_IRQn);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -211,11 +325,21 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, LED2_Pin|LED1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : LED2_Pin LED1_Pin */
   GPIO_InitStruct.Pin = LED2_Pin|LED1_Pin;
